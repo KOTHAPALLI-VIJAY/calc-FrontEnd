70e09502307c6bc026684cf61c4681ad63a4c8cc
KOTHAPALLI-VIJAY <vijaykothapalli26@gmail.com>
2025-08-25 10:40:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,33 @@ import { callApi, BASEURL } from './api';
 class App extends Component {
   constructor() {
     super();
-    this.state = { A: 0, B: 0, RES: 0 };
+    this.state = { A: 0, B: 0, RES: 0, error: "" };
     this.getResponse = this.getResponse.bind(this);
+    this.handleError = this.handleError.bind(this);
   }
 
   add() {
-    callApi("GET", BASEURL + `add/${this.state.A}/${this.state.B}`, "", this.getResponse);
+    callApi("GET", BASEURL + `add/${this.state.A}/${this.state.B}`, "", this.getResponse, this.handleError);
   }
 
   sub() {
-    callApi("GET", BASEURL + `sub/${this.state.A}/${this.state.B}`, "", this.getResponse);
+    callApi("GET", BASEURL + `sub/${this.state.A}/${this.state.B}`, "", this.getResponse, this.handleError);
   }
 
   mul() {
-    callApi("GET", BASEURL + `mul/${this.state.A}/${this.state.B}`, "", this.getResponse);
+    callApi("GET", BASEURL + `mul/${this.state.A}/${this.state.B}`, "", this.getResponse, this.handleError);
   }
 
   div() {
-    callApi("GET", BASEURL + `div/${this.state.A}/${this.state.B}`, "", this.getResponse);
+    callApi("GET", BASEURL + `div/${this.state.A}/${this.state.B}`, "", this.getResponse, this.handleError);
   }
 
   getResponse(res) {
-    this.setState({ RES: res });
+    this.setState({ RES: res, error: "" });
+  }
+
+  handleError(err) {
+    this.setState({ error: "⚠️ Failed to fetch result. Please check backend!" });
   }
 
   loadInputChange(event) {
@@ -34,64 +39,58 @@ class App extends Component {
   }
 
   render() {
-    const { A, B, RES } = this.state;
+    const { A, B, RES, error } = this.state;
     return (
       <>
-        <header className="header">
-          <div className='title'>✨ Simple Calculator ✨</div>
+        <header>
+          <div className="title">Simple Calculator</div>
         </header>
-
-        <section className="main">
-          <div className="card">
-            <table>
-              <tbody>
-                <tr>
-                  <td>Enter the value of A</td>
-                  <td>
-                    <input
-                      type='text'
-                      id='T1'
-                      name='A'
-                      value={A}
-                      onChange={(event) => this.loadInputChange(event)}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Enter the value of B</td>
-                  <td>
-                    <input
-                      type='text'
-                      id='T2'
-                      name='B'
-                      value={B}
-                      onChange={(event) => this.loadInputChange(event)}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Result</td>
-                  <td>
-                    <div className="result-box">{RES}</div>
-                  </td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td>
-                    <button className="add" onClick={() => this.add()}>ADD</button>
-                    <button className="sub" onClick={() => this.sub()}>SUB</button>
-                    <button className="mul" onClick={() => this.mul()}>MUL</button>
-                    <button className="div" onClick={() => this.div()}>DIV</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <section>
+          <table>
+            <tbody>
+              <tr>
+                <td>Enter the value of A</td>
+                <td>
+                  <input
+                    type="text"
+                    id="T1"
+                    name="A"
+                    value={A}
+                    onChange={(event) => this.loadInputChange(event)}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Enter the value of B</td>
+                <td>
+                  <input
+                    type="text"
+                    id="T2"
+                    name="B"
+                    value={B}
+                    onChange={(event) => this.loadInputChange(event)}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Result</td>
+                <td>
+                  {error ? <label style={{ color: "red" }}>{error}</label> : <label id="L1">{RES}</label>}
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <button onClick={() => this.add()}>ADD</button>&nbsp;
+                  <button onClick={() => this.sub()}>SUB</button>&nbsp;
+                  <button onClick={() => this.mul()}>MUL</button>&nbsp;
+                  <button onClick={() => this.div()}>DIV</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </section>
-
-        <footer className="footer">
-          © 2025 Simple Calculator | Designed with ❤️
-        </footer>
+        <footer>Copyright @ 2025. All rights reserved.</footer>
       </>
     );
   }
