5683d1c6afd74e5902b7e2d4dd42a9aff9b45a69
KOTHAPALLI-VIJAY <vijaykothapalli26@gmail.com>
2025-08-25 10:45:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,74 +9,89 @@ class App extends Component {
     this.getResponse = this.getResponse.bind(this);
   }
 
-  // ADD operation
   add() {
     callApi("GET", BASEURL + `add/${this.state.A}/${this.state.B}`, "", this.getResponse);
   }
 
-  // Callback for API response
+  sub() {
+    callApi("GET", BASEURL + `sub/${this.state.A}/${this.state.B}`, "", this.getResponse);
+  }
+
+  mul() {
+    callApi("GET", BASEURL + `mul/${this.state.A}/${this.state.B}`, "", this.getResponse);
+  }
+
+  div() {
+    callApi("GET", BASEURL + `div/${this.state.A}/${this.state.B}`, "", this.getResponse);
+  }
+
   getResponse(res) {
     this.setState({ RES: res });
   }
 
-  // Input change handler
   loadInputChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
     const { A, B, RES } = this.state;
-
     return (
       <>
-        <header>
-          <div className="title">Simple Calculator</div>
+        <header className="header">
+          <div className='title'>✨ Simple Calculator ✨</div>
         </header>
 
-        <section>
-          <table>
-            <tbody>
-              <tr>
-                <td>Enter the value of A</td>
-                <td>
-                  <input
-                    type="text"
-                    id="T1"
-                    name="A"
-                    value={A}
-                    onChange={(event) => this.loadInputChange(event)}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Enter the value of B</td>
-                <td>
-                  <input
-                    type="text"
-                    id="T2"
-                    name="B"
-                    value={B}
-                    onChange={(event) => this.loadInputChange(event)}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Result</td>
-                <td>
-                  <label id="L1">{RES}</label>
-                </td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>
-                  <button onClick={() => this.add()}>ADD</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <section className="main">
+          <div className="card">
+            <table>
+              <tbody>
+                <tr>
+                  <td>Enter the value of A</td>
+                  <td>
+                    <input
+                      type='text'
+                      id='T1'
+                      name='A'
+                      value={A}
+                      onChange={(event) => this.loadInputChange(event)}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Enter the value of B</td>
+                  <td>
+                    <input
+                      type='text'
+                      id='T2'
+                      name='B'
+                      value={B}
+                      onChange={(event) => this.loadInputChange(event)}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Result</td>
+                  <td>
+                    <div className="result-box">{RES}</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>
+                    <button className="add" onClick={() => this.add()}>ADD</button>
+                    <button className="sub" onClick={() => this.sub()}>SUB</button>
+                    <button className="mul" onClick={() => this.mul()}>MUL</button>
+                    <button className="div" onClick={() => this.div()}>DIV</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </section>
 
-        <footer>Copyright © 2025. All rights reserved.</footer>
+        <footer className="footer">
+          © 2025 Simple Calculator | Designed with ❤️
+        </footer>
       </>
     );
   }
